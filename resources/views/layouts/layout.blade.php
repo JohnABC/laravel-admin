963500a07d2f7048ceb41d700b83535999f91bcb
随永杰 <suiyongjie2008@gmail.com>
2020-03-10 12:55:29 +0800

    Laravel Admin

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
-    @php $laraveladminAssetsBaseUrl = app()['laraveladmin']-; @endphp
     <link media="all" type="text/css" rel="stylesheet" href="{{ \LaravelAdmin::assetsUrl('layui/css/layui.css') }}" />
     <link media="all" type="text/css" rel="stylesheet" href="{{ \LaravelAdmin::assetsUrl('style/admin.css') }}" />
 
@@ -20,7 +19,7 @@
 <body class="@yield('body-class', 'layui-layout-body')">
     @yield('body-content')
     @section('js-foot')
-        <script type="text/javascript" src="{{ \LaravelAdmin::assetsUrl('layui/layui.js') }}">
+        <script type="text/javascript" src="{{ \LaravelAdmin::assetsUrl('layui/layui.js') }}"></script>
     @show
     <script type="text/javascript">
         layui.config({

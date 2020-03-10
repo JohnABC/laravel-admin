74711368beb014d5f8f606290d996b614d679546
随永杰 <suiyongjie2008@gmail.com>
2020-03-10 12:34:19 +0800

    Laravel Admin

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,32 +2,29 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>@yield('title', env('APP_NAME' . '-后台管理系统'))</title>
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
-    @php
-        $laraveladminAssetsBaseUrl = app()['laraveladmin']->assetsUrl();
-        $laraveladminBasicCssUrl = $laraveladminAssetsBaseUrl . '/layui/css/layui.css';
-        $laraveladminAdminCssUrl = $laraveladminAssetsBaseUrl . '/style/admin.css';
-        $laraveladminBasicJsUrl = $laraveladminAssetsBaseUrl . '/layui/layui.js';
-    @endphp
+    @php $laraveladminAssetsBaseUrl = app()['laraveladmin']-; @endphp
+    <link media="all" type="text/css" rel="stylesheet" href="{{ \LaravelAdmin::assetsUrl('layui/css/layui.css') }}" />
+    <link media="all" type="text/css" rel="stylesheet" href="{{ \LaravelAdmin::assetsUrl('style/admin.css') }}" />
+
     @section('css')
-        @css({{ $laraveladminBasicCssUrl }})
-        @css({{ $laraveladminAdminCssUrl }})
     @show
     @section('js-title')
     @show
+
+    <title>@yield('title', env('APP_NAME' . '-后台管理系统'))</title>
 </head>
 <body class="@yield('body-class', 'layui-layout-body')">
     @yield('body-content')
     @section('js-foot')
-        @js({{  $laraveladminBasicJsUrl }})
+        <script type="text/javascript" src="{{ \LaravelAdmin::assetsUrl('layui/layui.js') }}">
     @show
     <script type="text/javascript">
         layui.config({
-            base: '@css({{ $laraveladminAssetsBaseUrl }})/'
+            base: '{{ \LaravelAdmin::assetsUrl() }}/'
         }).extend({
             index: 'lib/index'
         }).use('index');
